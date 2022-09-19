dff89eddffd17b3e5a5c37626dea1058d5a596ef
shako <davitashvilishako1@gmail.com>
2022-09-19 13:50:21 +0400

    Initial commit

---
+++ src/main/java/io/optimogroup/xracoon/xracoonnotification/XracoonNotificationApplication.java
@@ -0,0 +1,13 @@
+package io.optimogroup.xracoon.xracoonnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XracoonNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XracoonNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/optimogroup/xracoon/xracoonnotification/XracoonNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.optimogroup.xracoon.xracoonnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XracoonNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
