b5e9060a2b896ba9d52bf666fe67d4a9f44d7a6b
Irakli Bezhuashvili <i.bezhuashvili@optimogroup.io>
2022-12-15 22:27:37 +0400

    removed security

--- src/main/java/io/optimogroup/xracoon/xracoonnotification/config/SecurityConfiguration.java
+++ src/main/java/io/optimogroup/xracoon/xracoonnotification/config/SecurityConfiguration.java
@@ -1,23 +1,23 @@
-package io.optimogroup.xracoon.xracoonnotification.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-public class SecurityConfiguration {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-
-        return http.build();
-    }
-}
+//package io.optimogroup.xracoon.xracoonnotification.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@EnableWebSecurity
+//public class SecurityConfiguration {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//
+//        return http.build();
+//    }
+//}
--- src/main/java/io/optimogroup/xracoon/xracoonnotification/service/UserServiceImpl.java
+++ src/main/java/io/optimogroup/xracoon/xracoonnotification/service/UserServiceImpl.java
@@ -1,11 +1,6 @@
 package io.optimogroup.xracoon.xracoonnotification.service;
 
-import io.optimogroup.xracoon.xracoonnotification.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,13 +9,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String getCurrentUserId() {
-        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        Jwt token = jwtAuthenticationToken.getToken();
-        String userId = token.getClaim("user_id");
-        if (Utils.stringIsEmpty(userId)) {
-            log.error("Invalid User_id provided!");
-            throw new AccessDeniedException("Invalid User_id provided!");
-        }
-        return userId;
+//        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+//        Jwt token = jwtAuthenticationToken.getToken();
+//        String userId = token.getClaim("user_id");
+//        if (Utils.stringIsEmpty(userId)) {
+//            log.error("Invalid User_id provided!");
+//            throw new AccessDeniedException("Invalid User_id provided!");
+//        }
+        return "userId";
     }
 }
